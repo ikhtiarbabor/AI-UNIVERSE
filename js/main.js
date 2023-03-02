59a2ea3bfbbd6035dad212623fca17ed62c1c3ec
MOHAMMAD IKHTIAR UDDIN <ikhtiarbabor@gmail.com>
2023-03-03 01:26:16 +0600

    show more and show less button dibe

--- js/main.js
+++ js/main.js
@@ -1,35 +1,67 @@
-const loadData = async () => {
+const loadData = async (dataLimit) => {
   const url = 'https://openapi.programming-hero.com/api/ai/tools';
   const res = await fetch(url);
   const data = await res.json();
-  getAllData(data.data.tools);
+  getAllData(data.data.tools, dataLimit);
 };
-const getAllData = (data) => {
+const getAllData = (data, dataLimit) => {
   const featuresContainer = document.getElementById('features-container');
-  data.forEach((element) => {
-    const feature = document.createElement('div');
-    feature.classList.add('col-md-4', 'mb-5');
-    feature.innerHTML = `
-        <div class="card">
-          <div style="height: 18vw;" class="p-2 rounded">
-            <img src="${element.image}" class="card-img-top p-3 rounded" alt="..."/>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Features</h5>
-            <ol>
-                <li>${element.features[0]}</li>
-                <li>${element.features[1]}</li>
-                <li>${element.features[2]}</li>
-            </ol>
-            <hr />
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-    `;
-    featuresContainer.appendChild(feature);
-  });
+  const showMoreButton = document.getElementById('show-more-btn');
+
+  const eachData = (data) => {
+    data.forEach((element) => {
+      const feature = document.createElement('div');
+      feature.classList.add('col-md-4', 'mb-5');
+      feature.innerHTML = `
+            <div class="card">
+              <div style="height: 18vw;" class="p-2 rounded">
+                <img src="${element.image}" class="card-img-top p-3 rounded" alt="..."/>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Features</h5>
+                <ol>
+                    <li>${element.features[0]}</li>
+                    <li>${element.features[1]}</li>
+                    <li>${element.features[2]}</li>
+                </ol>
+                <hr class="m-0"/>
+              </div>
+              <div class="p-3">
+                <h4>${element.name}</h4>
+                <p>
+                    <i class="fa-regular fa-calendar-days"></i>
+                    <span>${element.published_in}</span>
+                </p>
+              </div>
+            </div>
+        `;
+      featuresContainer.appendChild(feature);
+    });
+  };
+  if (data.length > 6 && dataLimit === false) {
+    showMoreButton.classList.remove('d-none');
+    eachData(data.slice(0, 6));
+    const showLess = document.getElementById('show-less-btn');
+    showLess.classList.add('d-none');
+  } else {
+    eachData(data);
+    showMoreButton.classList.add('d-none');
+  }
 };
 
-loadData();
+// show more
+document.getElementById('show-more').addEventListener('click', function () {
+  const container = document.getElementById('features-container');
+  container.innerHTML = '';
+  const showLess = document.getElementById('show-less-btn');
+  showLess.classList.remove('d-none');
+  loadData(true);
+});
+// show less
+document.getElementById('show-less').addEventListener('click', function () {
+  const container = document.getElementById('features-container');
+  container.innerHTML = '';
+  loadData(false);
+});
+
+loadData(false);
